e77303ce532bd6b599a3381dcddbb8831ab24b07
Oyedele-Sg <Oyedelesegunfunmi@gmail.com>
2022-04-20 21:26:07 -0400

    Added email report

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, send_file
+from flask_mail import Mail, Message
 from flask_sqlalchemy import SQLAlchemy
 from pymysql import Time
 from sqlalchemy import exc, cast, Date, Time
@@ -12,6 +13,7 @@ from logging.config import dictConfig
 import xlsxwriter
 import os
 import csv
+import smtplib
 
 
 dictConfig(
@@ -46,6 +48,8 @@ load_dotenv()
 
 app = Flask(__name__)
 
+mail = Mail(app)
+
 # Database 
 driver = 'ODBC Driver 17 for SQL Server'
 user_name = os.getenv("USER_NAME")
@@ -55,12 +59,37 @@ password = os.getenv("DB_PASS")
 app.config["SQLALCHEMY_DATABASE_URI"] = f"mssql+pyodbc://{user_name}:{password}@{server}/{db_name}?driver={driver}"
 # app.config["SQLALCHEMY_DATABASE_URI"] = f"mssql+pyodbc://{server}/{db_name}?driver={driver}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_NATIVE_UNICODE'] = True
+# configuration of mail
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'oyedelesegunfunmi@gmail.com'
+app.config['MAIL_DEFAULT_SENDER'] = 'oyedelesegunfunmi@gmail.com'
+app.config['MAIL_PASSWORD'] = 'gdpmfostoussfscf'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+recipients = []
+for r in  os.getenv('ADMINS').split(','):
+    recipients.append(str(r))
+
+# app.config['MAIL_SERVER']='smtp.office365.com'
+# app.config['MAIL_PORT'] = 587
+# app.config['MAIL_USERNAME'] = 'segun@broadviewtechnicalsolutions.com'
+# app.config['MAIL_PASSWORD'] = '1minAb19Mood'
+# app.config['MAIL_USE_TLS'] = True
+# app.config['MAIL_USE_SSL'] = False
+mail = Mail(app)
+
+
 
 
 
 db = SQLAlchemy(app)
 
 Base = automap_base()
+
+
 
 def _name_for_collection_relationship(base, local_cls, referred_cls, constraint):
     if constraint.name:
@@ -85,7 +114,9 @@ def check_last_audit():
     if os.path.exists(file_path) and os.stat(file_path).st_size > 0:
         with open(file_path, 'r') as f:
             last_line = f.readlines()[-1]
-            last_audit = datetime.strptime(last_line.strip('\n'), "%Y-%m-%d %H:%M:%S.%f")
+            last_line = last_line.strip('\n')
+            if len(last_line) > 0:
+                last_audit = datetime.strptime(last_line.strip('\n'), "%Y-%m-%d %H:%M:%S.%f")
             return last_audit
     return last_audit
 
@@ -121,7 +152,7 @@ def generate_master_list_scan_codes(file_list):
 
 # Cross-reference scan codes 
 def get_unscanned_codes(master_scan_codes): 
-    last_hour = datetime.today() - timedelta(hours=2)
+    last_hour = datetime.today() - timedelta(hours=int(os.getenv('HOUR_THRESHOLD')))
     scan_codes = master_scan_codes.keys()
     db_scan_codes = session.query(OrderScans.SCANcode)
     db_scan_codes = db_scan_codes.filter(
@@ -131,30 +162,26 @@ def get_unscanned_codes(master_scan_codes):
     db_scan_codes = [r._asdict() for r in db_scan_codes]
     order_scans = [d['SCANcode'] for d in db_scan_codes]
     unscanned_codes = list(set(scan_codes) - set(order_scans))
-   
     return unscanned_codes
 
 # Get OrderTrackingID for packages without scan codes
-def get_order_tracking_ids(unscanned_codes):
+def get_order_tracking_ids():
     threshold =  datetime.today() - timedelta(days=14)
     threshold = threshold.date()
     db_orders = session.query(OrderPackageItems.OrderTrackingID, OrderPackageItems.RefNo)
     db_orders = db_orders.join(Orders, OrderPackageItems.OrderTrackingID == Orders.OrderTrackingID)
-    db_orders = db_orders.filter(
-        OrderPackageItems.RefNo.in_(unscanned_codes),
-        Orders.oDate.cast(Date) >= threshold
-        ).all()
+    db_orders = db_orders.filter(Orders.oDate.cast(Date) >= threshold).all()
     db_orders = [r._asdict() for r in db_orders]
-    orders = {}
+    unscanned_orders = {}
     for order in db_orders:
         k = order['RefNo']
         v = order['OrderTrackingID']
-        orders[k] = v
-    return orders
+        unscanned_orders[k] = v
+    return unscanned_orders
 
 
 # Generate audit report file
-def generate_audit_report(scan_codes, unscanned_orders):
+def generate_audit_report(master_scan_list, order_package_items, unscanned_codes):
 
     today = date.today()
     today = today.strftime("%m_%d_%y")
@@ -165,15 +192,15 @@ def generate_audit_report(scan_codes, unscanned_orders):
     headers = ['OrderTrackingID', 'ScanCode', 'TimeStamp']
     for x in range(len(headers)):
         worksheet.write(0, x, headers[x])
-
-    for idx, scan in enumerate(scan_codes.keys()):
-        if scan in unscanned_orders: 
-            worksheet.write(idx+1, 0, unscanned_orders[scan])
+    
+    for idx, scan in enumerate(unscanned_codes):
+        if scan in order_package_items: 
+            worksheet.write(idx+1, 0, order_package_items[scan])
         else:
             worksheet.write(idx+1, 0, 'None')
         worksheet.write(idx+1, 1, scan)
-        worksheet.write(idx+1, 2, scan_codes[scan])
-    
+        worksheet.write(idx+1, 2, master_scan_list[scan])
+
     workbook.close()
 
     # Write current timestamp to lastaudit
@@ -182,6 +209,18 @@ def generate_audit_report(scan_codes, unscanned_orders):
     with open(file_path, 'w') as f:
         f.write(str(datetime.now()))
         f.write('\n')
+
+    subject = 'Scan Audit - ' + today
+    msg = Message(
+                    subject,
+                    recipients = recipients
+                )
+    msg.body = 'Find attached the scan audit report in the email'
+    file = open(file_name, 'rb')
+  
+    
+    msg.attach(file_name, '	application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', file.read())
+    mail.send(msg)
 
     return send_file(
         file_name,
@@ -195,20 +234,32 @@ def home_rte():
     return render_template('home.html')
     
 
-@app.route('/report',  methods=["GET", "POST"])
-def report_rte():
+@app.route('/auditscan',  methods=["GET", "POST"])
+def audit_scan_rte():
     passcode=request.form.get("passcode")
     if passcode != os.getenv("PASSCODE"):
         return render_template('403.html')
 
     last_audit = check_last_audit()    
     file_list = generate_scan_file_list(last_audit)
-    scan_codes = generate_master_list_scan_codes(file_list)
-    unscanned_codes = get_unscanned_codes(scan_codes)
-    unscanned_orders = get_order_tracking_ids(unscanned_codes)
-    return generate_audit_report(scan_codes, unscanned_orders)
+    master_scan_list = generate_master_list_scan_codes(file_list)
+    unscanned_codes = get_unscanned_codes(master_scan_list)
+    order_package_items = get_order_tracking_ids()
     
+    return generate_audit_report(master_scan_list, order_package_items, unscanned_codes)
+   
+
+
+@app.route('/report',  methods=["GET", "POST"])
+def report_rte():
+
+    last_audit = check_last_audit()    
+    file_list = generate_scan_file_list(last_audit)
+    master_scan_list = generate_master_list_scan_codes(file_list)
+    unscanned_codes = get_unscanned_codes(master_scan_list)
+    order_package_items = get_order_tracking_ids()
     
+    return generate_audit_report(master_scan_list, order_package_items, unscanned_codes)
 
 if __name__ == "__main__":
     app.run()
